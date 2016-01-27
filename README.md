3a731ad8796b2cff7f7926c9805338e7b81deb7d
Raymond Chung <rchung.email@gmail.com>
2016-01-26 19:52:09 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+fibonacci
+===============
+
+Two implementations of the same fibonacci sequence generator, one using BDD and another using the classic development workflow.
+
+## Requirements
+'pip install -r requirements.txt'
+
+## How to Run
+
+BDD
+`cd fibonacci_bdd`
+`behave`
+
+Classic
+`cd fibonacci_old`
+`python tests/test_fibonacci.py`
---
+++ fibonacci_old/tests/test_fibonacci.py
@@ -0,0 +1,38 @@
+import sys
+sys.path.insert(
+    0, r'/Users/raymondchung/Desktop/Vagrant/Git/fibonacci/fibonacci_old')
+
+import os
+import unittest
+import fibonacci
+
+
+class fabFileTestCase(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test__fib_returns_array_of_integers(self):
+        mock_sequence = fibonacci.fib(7)
+        for i in mock_sequence:
+            assert isinstance(i, int)
+
+    def test__fib_returns_correct_array(self):
+        mock_sequence = fibonacci.fib(7)
+        actual_sequence = [1, 1, 2, 3, 5, 8, 13]
+        self.assertEquals(mock_sequence, actual_sequence)
+
+    def test__fib_returns_empty_array_for_negative(self):
+        mock_sequence = fibonacci.fib(-4)
+        actual_sequence = []
+        self.assertEquals(mock_sequence, actual_sequence)
+
+    def test__fib_throws_TypeError_for_strings(self):
+        with self.assertRaises(TypeError):
+            fibonacci.fib("pantheon")
+
+if __name__ == "__main__":
+    unittest.main()  # run all tests
